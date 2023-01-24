d398ae69b5fcfe9ce54722b2e7ff66f26282a8d0
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2023-01-24 14:38:50 -0500

    Working on the generic enemy scene

---
+++ cowboy-goes-east-game/enemy.gd
@@ -0,0 +1,32 @@
+extends KinematicBody2D
+
+const ACCELERATION = 750
+const MAX_SPEED = 325
+const FRICTION = 2800
+
+export (int) var health = 100
+var velocity = Vector2.ZERO
+export (int) var marg = 0
+var screen_size
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	move_state(delta)
+	position.x = clamp(position.x, marg, (screen_size.x - marg))
+
+func move_state(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
