617e97045ba3705b2df4d716c09aabcf304cc5b5
n0hb0dy <36122937+n0hb0dy@users.noreply.github.com>
2023-02-14 05:50:26 -0500

    Enemy scene improvements
    Improved the enemy scene with spawning, chasing, and hitboxes/hurtboxes. Also, removed the GIMP files since I am no longer using GIMP

--- cowboy-goes-east-game/scenes/enemies/Bat.gd
+++ cowboy-goes-east-game/scenes/enemies/Bat.gd
@@ -5,22 +5,32 @@ class_name Bat
 func _ready():
 	screen_size = get_viewport_rect().size
 	marg = 0
-	health = 1
+	health = 500
 	acceleration = 200
 	max_speed = 325
 	friction = 2800
 	velocity = Vector2.ZERO
+	spawn_delay = 2.0
+	spawn_radius = 500.0
+	
+	spawn()
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	#chase(delta, playerSingleton)
-	pass
-
-# Called in __physics_process() to translate the enem
-func __move_state(delta):
-	pass
+	._physics_process(delta)
 	
-func chase(delta, player):
-	velocity = velocity.move_toward(player.velocity, acceleration * delta)
-	velocity  = move_and_slide(velocity)
+	#FOR TESTING
+	health -= 1
+	
+	if health <= 0:
+		despawn()
+
+# Called when the enemy first spawns into the map
+func spawn():
+	self.global_position = Vector2(rand_range(-spawn_radius, spawn_radius), rand_range(-spawn_radius, spawn_radius))
+	.spawn()
+	
+# Called when the enemy is killed
+func despawn():
+	.despawn()
--- cowboy-goes-east-game/scenes/enemies/Enemy.gd
+++ cowboy-goes-east-game/scenes/enemies/Enemy.gd
@@ -1,20 +1,30 @@
 extends Actor
 class_name Enemy
 
+export var spawn_delay: float
+export var spawn_radius: float
+
+onready var player = get_node("/root/main/game/Player")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
 	velocity = Vector2.ZERO
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	
 func _physics_process(delta):
-	pass
+	var direction = (player.global_position - global_position).normalized()
+	velocity = velocity.move_toward(direction * max_speed, acceleration * delta)
+	move_and_slide(velocity)
+
+#For when an enemy spawns in (assumes it has a 'spawn' animation)
+func spawn():
+	var animation_player = self.get_node("AnimatedSprite")
+	animation_player.play("spawn")
 	
-# Called when the enemy first spawns into the map
-func spawn(delta):
-	pass
+# Called when the enemy is killed (assumes it has a 'despawn' animation)
+func despawn():
+	var animation_player = self.get_node("AnimatedSprite")
+	animation_player.play("despawn")
 	
-# Called when the enemy is killed
-func despawn(delta):
-	pass
+	queue_free()
